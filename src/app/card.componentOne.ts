b34f57a6cec1ec3d06501837bb5eb3fd7dddc086
1-Holopsicon-1 <mfnbrx@gmail.com>
2022-02-09 12:10:39 +0300

    Initial commit

---
+++ src/app/card.componentOne.ts
@@ -0,0 +1,45 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "card-comp",
+  template: `
+    <table>
+      <tr>
+        <th>id</th>
+        <th>title</th>
+        <th>author</th>
+        <th>genre</th>
+        <th>description</th>
+        <th>image</th>
+        <th>published</th>
+        <th>publisher</th>
+      </tr>
+      <tr>
+        <td>{{id}}</td>
+        <td>{{title}}</td>
+        <td>{{author}}</td>
+        <td>{{genre}}</td>
+        <td style="width: 100px">{{description}}</td>
+        <td><img src="{{image}}" style="width: 20px" height="20px"></td>
+        <td>{{published}}</td>
+        <td>{{publisher}}</td>
+      </tr>
+    </table>
+  `,
+  styles: [`table, th, td {
+    border:1px solid black;
+  }`]
+
+})
+
+export class Card {
+  id: number =  1;
+  title: string = "In a little.";
+  author: string = "Wyatt Connelly";
+  genre: string = "Tempore";
+  description: string = "Hatter, it woke up again as quickly as she went back to finish his story. CHAPTER IV. The Rabbit started violently, dropped the white kid gloves while she was considering in her head, and she jumped.";
+  image: string = "http://placeimg.com/480/640/any";
+  published: string = "2013-06-24";
+  publisher: string = "Doloremque Amet";
+}
+
